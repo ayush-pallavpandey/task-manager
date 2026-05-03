a80986fc19c226b45d63df80be1be7dc87dc67bb
AYUSH PALLAV PANDEY <148118167+ayush-pallavpandey@users.noreply.github.com>
2026-05-03 18:47:57 +0530

    Add files via upload

---
+++ SayHelloController.java
@@ -0,0 +1,15 @@
+package com.in28minute.springboot.myfirstwebapp.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class SayHelloController {
+
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String sayHello() {
+        return "Hello controller";
+    }
+}
